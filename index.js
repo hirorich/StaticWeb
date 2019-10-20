d61dc7f6e8fbcc2e7f3f649ca3c598ae4e67be1a
hirorich <ch.dim7ir@gmail.com>
2019-10-20 21:56:14 +0900

    作成したサンプルページへのリンク集

---
+++ index.js
@@ -0,0 +1,20 @@
+// Vue.js オブジェクト作成
+var vm=new Vue({
+    el:'#app',
+    data:{
+        items:[
+            {
+                name:'Vue',
+                url:'./Vue/'
+            },
+            {
+                name:'Table',
+                url:'./Table/'
+            },
+            {
+                name:'CheckTable',
+                url:'./CheckTable/'
+            }
+        ]
+    }
+});
