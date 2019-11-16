df1a084d9086c006be8d359e6b51952b740f401c
hirorich <ch.dim7ir@gmail.com>
2019-11-16 22:20:44 +0900

    シャッフルのロジック修正

--- Transition/index.js
+++ Transition/index.js
@@ -1,8 +1,22 @@
+// リストをシャッフル
+var shuffle=function(array){
+    for (var i=array.length-1; i>0; i--){
+        var r = Math.floor(Math.random() * (i+1))
+        if(r>=(i+1)){
+            r=i
+        }
+
+        array.splice(i+1, 0, array[r])
+        array.splice(r, 1)
+    }
+
+}
+
 // Vue.js オブジェクト作成
 var vm=new Vue({
     el: '#app',
     data: {
-        items: [1,2,3,4,5,6,7,8,9],
+        items: [0,1,2,3,4,5,6,7,8,9],
         nextNum: 10
     },
     methods: {
@@ -16,19 +30,7 @@ var vm=new Vue({
             this.items.splice(this.randomIndex(), 1)
         },
         shuffle: function () {
-            var tmp_item = this.items
-            var tmp_list = []
-            var max_count=tmp_item.length
-            var index=0
-            for (var i=0; i<max_count; i++){
-                index = Math.floor(Math.random() * tmp_item.length)
-                if(index>=tmp_item.length){
-                    index=tmp_item.length - 1
-                }
-                tmp_list.splice(i, 0, tmp_item[index])
-                tmp_item.splice(index, 1)
-            }
-            this.items=tmp_list
+            shuffle(this.items)
         }
     }
 });
