97128838c522e9c1758c153a8595259f795080b9
hirorich <ch.dim7ir@gmail.com>
2019-12-31 16:03:06 +0900

    リストへの要素動的追加

---
+++ v-modelText/index.js
@@ -0,0 +1,35 @@
+// Vue.js オブジェクト作成
+var inputinfo=new Vue({
+    el:'#inputinfo',
+    data:{
+        info:{
+            filename:"",
+            link:""
+        }
+    },
+    methods:{
+        push: function(){
+            var data={
+                filename:this.info.filename,
+                link:this.info.link
+            }
+            vm.items.splice(vm.items.length, 0, data);
+            this.info.filename="";
+            this.info.link="";
+        }
+    }
+});
+
+var vm=new Vue({
+    el:'#app',
+    data:{
+        items:[]
+    },
+    methods:{
+        del: function(index){
+            if (index >= 0 && index <this.items.length) {
+                this.items.splice(index, 1);
+            }
+        }
+    }
+});
